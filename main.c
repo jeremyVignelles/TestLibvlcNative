5c2c06a323b58c0b74f0ac916647a4e8e8d2417a
Jérémy VIGNELLES <jeremy.vignelles@dev3i.fr>
2019-04-12 16:24:17 +0200

    Added a sample for reproducing the seek bug while playing a MPEG-TS in a zip

--- main.c
+++ main.c
@@ -8,12 +8,16 @@ int main(int argc, const char **argv)
     libvlc_instance_t* vlc = libvlc_new(1, options);
     libvlc_media_player_t* media_player = libvlc_media_player_new(vlc);
 
-    libvlc_media_t* media = libvlc_media_new_location(vlc, "http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi");
+    libvlc_media_t* media = libvlc_media_new_location(vlc, "file:///D:\\Projects\\Tests\\TestLibvlcNative\\bin\\video.zip#!/video.ts");
 
     libvlc_media_player_set_media(media_player, media);
     libvlc_media_release(media);
 
     libvlc_media_player_play(media_player);
+
+    system("PAUSE");
+    printf("length : %ld \n", libvlc_media_player_get_length(media_player));
+    libvlc_media_player_set_time(media_player, 10000);
 
     system("PAUSE");
 
