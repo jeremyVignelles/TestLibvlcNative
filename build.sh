f19053710c60e5478fbbd471d21e142f675a788f
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2019-07-17 22:05:14 +0200

    4.0 base branch

--- build.sh
+++ build.sh
@@ -2,23 +2,24 @@
 
 cd `dirname "$0"`
 
-VLC_VERSION=3.0.7.1
+VLC_VERSION=4.0.0-20190717-0833
+VLC_FOLDER_NAME=vlc-4.0.0-dev
 
-if [[ ! -d vlc-$VLC_VERSION ]]; then
-    wget https://get.videolan.org/vlc/$VLC_VERSION/win64/vlc-$VLC_VERSION-win64.7z
+if [[ ! -d $VLC_FOLDER_NAME ]]; then
+    wget https://nightlies.videolan.org/build/win64/vlc-$VLC_VERSION/vlc-$VLC_VERSION-dev-win64.7z
 
-    7z x vlc-$VLC_VERSION-win64.7z
+    7z x vlc-$VLC_VERSION-dev-win64.7z
 fi
 
 
 if [[ ! -d bin ]]; then
     mkdir bin
-    cp vlc-$VLC_VERSION/libvlc.dll bin
-    cp vlc-$VLC_VERSION/libvlccore.dll bin
-    cp -r vlc-$VLC_VERSION/hrtfs bin
-    cp -r vlc-$VLC_VERSION/locale bin
-    cp -r vlc-$VLC_VERSION/lua bin
-    cp -r vlc-$VLC_VERSION/plugins bin
+    cp $VLC_FOLDER_NAME/libvlc.dll bin
+    cp $VLC_FOLDER_NAME/libvlccore.dll bin
+    cp -r $VLC_FOLDER_NAME/hrtfs bin
+    cp -r $VLC_FOLDER_NAME/locale bin
+    cp -r $VLC_FOLDER_NAME/lua bin
+    cp -r $VLC_FOLDER_NAME/plugins bin
 fi
 
-x86_64-w64-mingw32-gcc main.c -I/src/vlc-$VLC_VERSION/sdk/include -L/src/vlc-$VLC_VERSION/sdk/lib -lvlccore -lvlc -o bin/testVlc.exe
+x86_64-w64-mingw32-gcc main.c -I/src/$VLC_FOLDER_NAME/sdk/include -L/src/$VLC_FOLDER_NAME/sdk/lib -lvlccore -lvlc -o bin/testVlc.exe
